8c09e0cd558d00d221a4c09944744664c35cc540
GlooMy03 <nandaadela2903@gmail.com>
2024-10-30 15:59:50 +0700

    login dan register sudah by nanda

--- lib/app/modules/signup/controllers/signup_controller.dart
+++ lib/app/modules/signup/controllers/signup_controller.dart
@@ -1,23 +1,73 @@
+import '../../home/views/home_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../login/views/login_view.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class SignupController extends GetxController {
-  //TODO: Implement SignupController
+class SignUpController extends GetxController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  RxBool isLoading = false.obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  // Fungsi untuk registrasi pengguna
+  Future<void> registerUser(String email, String password, String username, String phoneNumber) async {
+    try {
+      isLoading.value = true;
+
+      // Proses registrasi dengan email dan password
+      final userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Jika berhasil, simpan data pengguna ke Firestore
+      await _saveUserDataToFirestore(username, phoneNumber, userCredential.user?.uid);
+
+      // Tampilkan pesan sukses dan arahkan ke HomeView
+      Get.snackbar('Success', 'Registration successful', backgroundColor: Colors.green);
+      Get.off(HomeView());
+    } catch (authError) {
+      // Tampilkan pesan error jika registrasi di Firebase Authentication gagal
+      Get.snackbar('Error', 'Registration failed: $authError', backgroundColor: Colors.red);
+    } finally {
+      isLoading.value = false;
+    }
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  // Fungsi untuk login pengguna
+  Future<void> loginUser(String email, String password) async {
+    try {
+      isLoading.value = true;
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      Get.snackbar('Success', 'Login successful', backgroundColor: Colors.green);
+      Get.off(HomeView()); // Navigasi ke HomeView setelah login berhasil
+    } catch (error) {
+      Get.snackbar('Error', 'Login failed: $error', backgroundColor: Colors.red);
+    } finally {
+      isLoading.value = false;
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  // Fungsi untuk logout
+  void logout() async {
+    await _auth.signOut();
+    Get.offAll(LoginView());
   }
 
-  void increment() => count.value++;
+  // Helper function untuk menyimpan data pengguna ke Firestore
+  // Helper function untuk menyimpan data pengguna ke Firestore
+Future<void> _saveUserDataToFirestore(String username, String phoneNumber, String? uid) async {
+    if (uid != null) {
+      await FirebaseFirestore.instance.collection('users').doc(uid).set({
+        'username': username,
+        'phoneNumber': phoneNumber,
+        'email': _auth.currentUser?.email,
+        'uid': uid,
+      });
+    }
+}
+
 }
--- lib/app/modules/signup/views/signup_view.dart
+++ lib/app/modules/signup/views/signup_view.dart
@@ -1,126 +1,146 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:myapp/app/routes/app_pages.dart';
 import '../controllers/signup_controller.dart';
 
-class SignupView extends GetView<SignupController> {
+class SignupView extends GetView<SignUpController> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final usernameController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  SignupView() {
+    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> showNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'signup_channel',
+      'Signup Notifications',
+      importance: Importance.high,
+      priority: Priority.high,
+    );
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(0, 'Sign Up Berhasil', 'Selamat datang di Kaza Kopi Nusantara!', platformChannelSpecifics);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView( // Tambahkan SingleChildScrollView
+      body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Logo and Title
               Center(
                 child: Column(
                   children: [
-                    // Logo
-                    Image.asset('assets/LOGO.png',
-                        height: 100), // Ensure the correct logo path
+                    Image.asset('assets/LOGO.png', height: 100),
                     SizedBox(height: 30),
-
-                    // App Name
                     Text(
                       "Kaza Kopi Nusantara",
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey[800],
-                      ),
+                      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.grey[800]),
                     ),
                     SizedBox(height: 10),
-
-                    // Subtitle for Sign In
-                    Text(
-                      "Welcome Back! Please sign in.",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.grey[700],
-                      ),
-                    ),
+                    Text("Welcome! Please sign Up.", style: TextStyle(fontSize: 18, color: Colors.grey[700])),
                   ],
                 ),
               ),
-
               SizedBox(height: 40),
-
-              // Username TextField
               TextField(
+                controller: usernameController,
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
                   labelText: 'Username',
                   prefixIcon: Icon(Icons.person),
                   hintText: 'Enter your username',
                 ),
               ),
               SizedBox(height: 20),
-
-              // Phone Number TextField
               TextField(
+                controller: phoneController,
                 keyboardType: TextInputType.phone,
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
                   labelText: 'Phone Number',
                   prefixIcon: Icon(Icons.phone),
                   hintText: 'Enter your phone number',
                 ),
               ),
               SizedBox(height: 20),
-
-              // Email TextField
               TextField(
+                controller: emailController,
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
                   labelText: 'Email',
                   prefixIcon: Icon(Icons.email),
                   hintText: 'Enter your email',
                 ),
               ),
               SizedBox(height: 20),
-
-              // Password TextField
               TextField(
+                controller: passwordController,
                 obscureText: true,
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
                   labelText: 'Password',
                   prefixIcon: Icon(Icons.lock),
                   hintText: 'Enter your password',
                 ),
               ),
-
               SizedBox(height: 20),
-
-              // Sign-In Button
-              ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.HOME);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.grey[800], // Button color
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                ),
-                child: Text(
-                  'Sign In',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
+              Obx(() {
+                return ElevatedButton(
+                  onPressed: controller.isLoading.value
+                      ? null
+                      : () async {
+                          if (emailController.text.isNotEmpty &&
+                              passwordController.text.isNotEmpty &&
+                              usernameController.text.isNotEmpty &&
+                              phoneController.text.isNotEmpty) {
+                            try {
+                              await controller.registerUser(
+                                emailController.text,
+                                passwordController.text,
+                                usernameController.text,
+                                phoneController.text,
+                              );
+                              await showNotification();
+                              Get.toNamed(Routes.LOGIN);
+                            } catch (error) {
+                              Get.snackbar(
+                                'Error',
+                                'Sign Up failed: $error',
+                                backgroundColor: Colors.red,
+                              );
+                            }
+                          } else {
+                            Get.snackbar(
+                              'Error',
+                              'Semua kolom harus diisi',
+                              backgroundColor: Colors.red,
+                            );
+                          }
+                        },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[800],
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                   ),
-                ),
-              ),
-
+                  child: controller.isLoading.value
+                      ? CircularProgressIndicator(color: Colors.white)
+                      : Text(
+                          'Sign Up',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                );
+              }),
               SizedBox(height: 20),
             ],
           ),
